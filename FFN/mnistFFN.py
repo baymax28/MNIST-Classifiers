07025e638b2bbe4a2829a6eda24b12d39add5e12
komalkum <komal.kumar@oracle.com>
2017-09-23 16:16:05 +0530

    Added FNN folder and corresponding files.
    This reverts commit 473ae3dabd4c9e72bb59ca5b112dff25b5d15e5d.

---
+++ FFN/mnistFFN.py
@@ -0,0 +1,38 @@
+import tensorflow as tf 
+
+NUM_CLASSES = 10
+
+IMAGE_SIZE = 28
+IMAGE_PIXELS = IMAGE_SIZE * IMAGE_SIZE
+
+def feed_forward_model(images, hidden1_nodes, hidden2_nodes):
+	
+	with tf.name_scope('Hidden1'):
+		weights = tf.Variable(tf.random_normal([IMAGE_PIXELS, hidden1_nodes]), name='weights')
+		biases = tf.Variable(tf.random_normal(hidden1_nodes), name='biases')
+		hidden1 = tf.nn.relu(tf.add(tf.matmul(images, weights), biases))
+
+	with tf.name_scope('Hidden2'):
+		weights = tf.Variable(tf.random_normal([hidden1_nodes, hidden2_nodes]), name='weights')
+		biases = tf.Variable(tf.random_normal(hidden2_nodes), name='biases')
+		hidden2 = tf.nn.relu(tf.add(tf.matmul(hidden1, weights), biases))
+
+	with tf.name_scope('Logits'):
+		weights = tf.Variable(tf.random_normal([hidden2_nodes, NUM_CLASSES]), name='weights')
+		biases = tf.Variable(tf.random_normal(NUM_CLASSES), name='biases')
+		logits = tf.add(tf.matmul(hidden2, weights), biases)
+
+	return logits
+
+def loss(logits, labels):
+	loss_tensor = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=labels, logits=logits)
+	loss = tf.reduce_mean(loss_tensor)
+	return loss
+
+def training(loss, learning_rate):
+	optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+	optimizer.minimize(loss)
+
+def evaluation(logits, labels):
+	correct = tf.nn.in_top_k(logits, labels, k=1)
+	return tf.reduce_sum(tf.cast(correct, tf.int32))
